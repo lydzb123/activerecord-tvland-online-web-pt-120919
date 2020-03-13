09729d0300fc33759d04a88718673f67e5dfbef9
Lydia B <lydiabaik@gmail.com>
2020-03-13 20:01:08 +0000

    added associations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,11 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
 def full_name
   full_name = []
   full_name << self.first_name
   full_name << self.last_name
   full_name
 end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,4 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actor, through: :characters
 end
